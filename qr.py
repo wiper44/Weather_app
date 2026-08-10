2731c6cd544520d0d8cc755b7cb4d98647171995
Shyam shyam <shyamashyamupreti@gmail.com>
2026-08-10 10:48:28 +0545

    Delete qr.py

--- qr.py
+++
@@ -1,8 +0,0 @@
-#intall qr code module for the qr code import 
-
-import qrcode as qr
-from PIL import Image
-#function to generate qr code & the link is attached.
-img = qr.make("https://github.com/cracktivities1/Python")
-#image created 
-img.save("qrcode.png")
